b3df35d58d776f0d7ad557118fc662f11b1b4383
ccaiazzo <cirodonato.caiazzo@coesia.com>
2021-04-06 19:20:07 +0200

    FlatList

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React, {useState} from 'react';
-import { StyleSheet, Text, View, ScrollView } from 'react-native';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
 
 export default function App() {
   const [people, setPeople] = useState([
@@ -16,7 +16,17 @@ export default function App() {
 
   return (
     <View style={styles.container}> 
-    <ScrollView>
+
+    <FlatList
+    numColumns={2}
+    keyExtractor={(item) => item.key } // e.g. item.id 
+      data={people}
+      renderItem={({ item }) =>(
+        <Text style={styles.item}>{item.name}</Text>
+
+      )}
+    />
+    {/* <ScrollView>
     {
       people.map(item => (
           <View key={item.key}>
@@ -24,7 +34,7 @@ export default function App() {
           </View>
         ))
     }
-    </ScrollView>
+    </ScrollView> */}
     </View>
   );
 }
@@ -42,6 +52,8 @@ const styles = StyleSheet.create({
     marginTop: 24,
     padding:30,
     backgroundColor: 'pink',
-    fontSize:24
+    fontSize:24,
+    marginHorizontal:10,
+    marginTop:24,
   }
 });
