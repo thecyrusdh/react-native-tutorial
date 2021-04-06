df883e31dcf46e95c04a89b47af26dda35671305
ccaiazzo <cirodonato.caiazzo@coesia.com>
2021-04-06 16:13:45 +0200

    use state and button

--- App.js
+++ App.js
@@ -1,14 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
+  const [name, setName] = useState('shaun');
+  const [person, setPerson] = useState({name: 'mario', age: 40});
+  const clickHandler = () =>{
+    setName('chun-li'); 
+    setPerson({name:'luigi', age:45});
+  }
+
+
   return (
     <View style={styles.container}>
-      <Text></Text>
-      <Text></Text>
-      <Text></Text>
-   
+      <Text>My name is {name}</Text>
+      <Text>His name is {person.name} and his hage is {person.age}</Text>
+      <View style={style.buttonContainer}>
+        <Button title='update state' onPress={clickHandler}/>
+      </View>
     </View>
   );
 }
@@ -20,4 +29,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  buttonContainer: {
+    marginTop: 20
+  }
 });
