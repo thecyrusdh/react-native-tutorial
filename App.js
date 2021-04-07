bb56f4af87d148e86d6abcb7063c95a4527c738a
ccaiazzo <cirodonato.caiazzo@coesia.com>
2021-04-07 19:48:37 +0200

    step 2

--- App.js
+++ App.js
@@ -2,13 +2,20 @@ import { StatusBar } from 'expo-status-bar';
 import React, {useState} from 'react';
 import { StyleSheet, Text, View, FlatList  } from 'react-native';
 import Header from './components/header'
+import TodoItem from './components/todoItem'
 
 export default function App() {
   const[todos, setTodos] = useState([
-    {text:'buy coffee', key:'1'},
+    {text:'buy coffee', key:'1'}, 
     {text:'create an app', key:'2'},
     {text:'play on the switch', key:'3'},
   ]);
+
+  const pressHandler = (key) => {
+    setTodos((prevTodos) =>{
+      return prevTodos.filter(todo => todo.key != key);
+    });
+  }
 
   return (
     <View style={styles.container}> 
@@ -19,7 +26,7 @@ export default function App() {
           <FlatList
             data={todos}
             renderItem={({ item }) => (
-              <Text>{item.text}</Text>
+              <TodoItem  item={item} pressHandler={pressHandler}/>
             )}
           />
         </View>
