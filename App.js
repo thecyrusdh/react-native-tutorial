dee060d3b270d3e9be8cf61e33aef1803debdbec
ccaiazzo <cirodonato.caiazzo@coesia.com>
2021-04-16 10:17:41 +0200

    keyboard dismiss

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React, {useState} from 'react';
-import { StyleSheet, Text, View, FlatList, Alert  } from 'react-native';
+import { StyleSheet, Text, View, FlatList, Alert, TouchableWithoutFeedback, Keyboard  } from 'react-native';
 import Header from './components/header'
 import TodoItem from './components/todoItem'
 import AddTodo from './components/addTodo'
@@ -36,20 +36,25 @@ export default function App() {
   }
 
   return (
-    <View style={styles.container}> 
-      <Header/>
-      <View style={styles.content}>
-        <AddTodo submitHandler= {submitHandler}/>
-        <View style={styles.list}>
-          <FlatList
-            data={todos}
-            renderItem={({ item }) => (
-              <TodoItem  item={item} pressHandler={pressHandler}/>
-            )}
-          />
+    <TouchableWithoutFeedback onPress={() => {
+      Keyboard.dismiss();
+      console.log('dismissed keyboard');
+      }}>      
+      <View style={styles.container}> 
+        <Header/>
+        <View style={styles.content}>
+          <AddTodo submitHandler= {submitHandler}/>
+          <View style={styles.list}>
+            <FlatList
+              data={todos}
+              renderItem={({ item }) => (
+                <TodoItem  item={item} pressHandler={pressHandler}/>
+              )}
+            />
+          </View>
         </View>
       </View>
-    </View>
+    </TouchableWithoutFeedback>
   );
 }
 
