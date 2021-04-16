7aaba84bd1452f61f931f92a57fc0b7b332b1d5a
ccaiazzo <cirodonato.caiazzo@coesia.com>
2021-04-16 10:11:16 +0200

    Alert

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React, {useState} from 'react';
-import { StyleSheet, Text, View, FlatList  } from 'react-native';
+import { StyleSheet, Text, View, FlatList, Alert  } from 'react-native';
 import Header from './components/header'
 import TodoItem from './components/todoItem'
 import AddTodo from './components/addTodo'
@@ -19,12 +19,20 @@ export default function App() {
   }
 
   const submitHandler = (text) => {
-    setTodos((prevTodos) =>{
-      return [
-        {text: text, key: Math.random().toString()},
-        ...prevTodos
-      ]
-    })
+    if(text.length >3) {
+      setTodos((prevTodos) =>{
+        return [
+          {text: text, key: Math.random().toString()},
+          ...prevTodos
+        ]
+      }) 
+
+      }else {
+        Alert.alert('OOPS!', 'Todos must be over 3 charts long', [
+          {text:'Undestood', onPress: () => console.log('alert closed')}
+        ])
+    }
+    
   }
 
   return (
